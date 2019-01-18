a47934321574448db3ef6ba17cae1e2673839eef
Laurine <30570842+LafLaurine@users.noreply.github.com>
2019-01-18 14:12:35 +0100

    Delete histogram.h

--- histogram.h
+++
@@ -1,9 +0,0 @@
-#ifndef HISTOGRAM_H
-#define HISTOGRAM_H
-
-#include "image.h"
-
-int init_histo(PPMImage* img, PPMImage* histogramme);
-void _printHistogram(PPMImage* histogram, unsigned int* histogramData, unsigned int maxData, unsigned char printColor);
-
-#endif 
